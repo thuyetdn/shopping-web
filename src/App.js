e9e504218078c7361a9b64aaebf125c7e49e2327
17021056 <thuyettheory123@gmail.com>
2021-01-16 15:55:02 +0700

    create-filter-productlist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import React, {useState} from "react";
+import Filter from "./components/Filter";
 import Product from "./components/Product";
 import data from "./data.json"
 function App() {
   const [products,setProducts] = useState(data.products)
   const [size,setSize] = useState("")
   const [sort,setSort] = useState("")
+  const  filterProducts = (event) =>{
+    if (event.target.value === "") {
+      setSize(event.target.value)
+      setProducts(data.products)
+    } else {
+      setSize(event.target.value)
+      setProducts( data.products.filter(
+          (product) => 
+          product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      )
+    }
+  } 
+  const sortProducts = (event) =>{
+    // const sort = event.target.value;
+    setSort(sort)
+    // console.log(sort)
+    setProducts(products.slice().sort(
+      (a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        )
+    )
+    console.log(products)
+  }
+
   return (
     <div className="grid-container">
       <header className="">
@@ -13,6 +49,13 @@ function App() {
       <main>
           <div className="content">
               <div className="main">
+                 <Filter 
+                        count={products.length}
+                        size={size}
+                        sort={sort}
+                        filterProducts={filterProducts}
+                        sortProducts={sortProducts}>
+                  </Filter>
                  <Product products={products}></Product>
               </div>
               <div className="sidebar">
