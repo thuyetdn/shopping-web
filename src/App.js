743db3234db0486b11addce661ebd1509ca9ed87
17021056 <thuyettheory123@gmail.com>
2021-01-16 11:21:48 +0700

    products-list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-//feature 1
+import React, {useState} from "react";
+import Product from "./components/Product";
+import data from "./data.json"
 function App() {
+  const [products,setProducts] = useState(data.products)
+  const [size,setSize] = useState("")
+  const [sort,setSort] = useState("")
   return (
     <div className="grid-container">
       <header className="">
           <a href = "/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+          <div className="content">
+              <div className="main">
+                 <Product products={products}></Product>
+              </div>
+              <div className="sidebar">
+                Cart items
+              </div>
+          </div>
       </main>
       <footer>
         All right is reserved
