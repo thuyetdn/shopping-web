03cc1ba44c61fdb35d0c125a2e3a81da298b5712
17021056 <thuyettheory123@gmail.com>
2021-01-16 14:33:27 +0700

    create product list

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,9 +4,9 @@ function Product({products}){
      const product = products.map((product) => (
           <li key={product._id}>
           <div className="product">
-               <a href={"#" + product._id}>
+               <a  href={"#" + product._id}>
                <img src={product.image} alt={product.title}></img>
-               <p>{product.title}</p>
+               <p className="product-name">{product.title}</p>
                </a>
                <div className="product-price">
                <div>{formatCurrency(product.price)}</div>
